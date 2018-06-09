ff21dc1ce747a99b1799cec9531b06ce92440a3c
ahmedfathy17 <ahmed.fathy.elbossily@gmail.com>
2018-06-09 14:31:45 +0200

    Modify Main.cpp file

--- mps/main.cpp
+++ mps/main.cpp
@@ -16,6 +16,202 @@ double _x1=(1.0-_x0)/2.0;
 double _min_angle=90;
 double _max_angle=90;
 
+int odd (int a);
+double dist_two_points_3d(double x1,double y1,double z1,double x2,double y2,double z2);
+double random_number();
+void  mps_3d(double *points_x,double *points_y,double *points_z,int num_points,double r,int &num_itr,int initiation);
+void  regular_points_seeds(double *points_x,double *points_y,double *points_z,double dist);
+double max(double d_1,double d_2);
+void trim(float &x1 ,float &y1,float &z1,float &x2,float &y2,float &z2,int &boundary,int &draw_check);
+void draw_cells(double *draw_points, int num_draw_points,string &str);
+void one_cell_draw(tetgenio &out,int &draw_check,string str);
+void Voronoi_vertices_inside_unite_cube_extraction(tetgenio &out,int &draw_check,double &total_distance,
+												   double * draw_points,int * draw_check_array,int &num_draw_points,
+												float &x1,float&y1,float&z1,float&x2,float&y2,float&z2,int &draw_index,int &point_index);
+void small_ligm_remove(tetgenio &out,int &draw_check,double beam_rad,int* ch_p_posi,int &p,int &num_small_lig);
+void tet_gen(int num_points,double *points_x,double*points_y,double*points_z,double &beam_rad,double denesity,
+			 double r_reg,double e,int initiation,double r,int &num_small_lig,int &num_draw_points,double * draw_points);
+void tet_gen_honeycomb(int num_points,double *points_x,double*points_y,double*points_z,
+					   double &beam_rad,double denesity,int &num_draw_points,double * draw_points);
+double min_dist_req_3d(double V,int num_points);
+double random_number( double start,double interval );
+void  MPS_complete_regular_bycub(double *points_x,double *points_y,double *points_z,double x0, 
+								 double y0, double z0,double dist, int &hit, double disk_raduis, int se );
+int covered(double *child_cub,double *points_x,double *points_y,double *points_z, double disk_raduis,int hit);
+void child_setup(double *child_cub,double x,double y,double z,double dist);
+void pearants(int step,double *AAA,int hit,double *points_x,double *points_y,
+			  double *points_z,double dist,double *empty_cubs,int &empty_index);
+void  cube_corners(double *corner,double x,double y,double z,double dist, int level);
+void edit_empty_cubs(double *empty_cubs,int &empty_index);
+int seedstracking_lastparent(int &hit,double *points_x,double *points_y, double *points_z, double disk_raduis);
+void  mps_3d_for_high_regularity(double *points_x,double *points_y,double *points_z,int num_points,double r,
+								 int &num_itr,int initiation);
+
+void main()
+{	
+	// creat array that holds voroni vertices 
+	double * draw_points;
+	draw_points= new double [60000000];
+	int num_draw_points=0;
+	
+	//Create a parameter to check whether the user want to generate a honeycomb structure 1 or random voronoi diagram 
+	int reg_type = 1;
+	
+	//the required number of seed inside the unite cube 
+	int num_points=559;
+	int num_itr=1;
+	double* points_x, * points_y, * points_z;
+	
+	// Create variables that holds Vornoi ligaments characteristics
+	double beam_rad=0.0;
+	int num_small_lig=0;
+	double denesity;
+
+	// calculate the required distance to generate complete regulare voronoi structure for "num_points" seeds in unite cube "_x0*_x0*_x0"
+	double r_reg;
+	r_reg=min_dist_req_3d(_x0*_x0*_x0,num_points);
+
+	//in case of user want to generate complete regulare structure "honeycomb"
+	if(reg_type==1)
+	{
+		// set the the required density of model
+		denesity=0.03;
+
+		// Create arrays holds seeds coordinates 
+		points_x = new double [num_points];
+		points_y = new double [num_points];
+		points_z = new double [num_points];
+
+		// generate regulare distribution seeds 
+		regular_points_seeds(points_x,points_y,points_z,r_reg);
+
+		// Creating Vornoi vertices for honeycomb structure using TetGen library 
+		tet_gen_honeycomb(num_points,points_x,points_y,points_z,beam_rad, denesity,num_draw_points,draw_points);
+
+		//set the destination for storing DXF file 
+		string str="D:\\master\\results\\1reg\\00_Drawing.dxf";
+
+		// Draw Honeycomb structure in dxf file
+		draw_cells(draw_points,num_draw_points,str);
+
+		//display voronoi ligaments radiaus in inch and in m
+		cout<<"beam_rad	:"<<beam_rad<<"	inch"<<endl;
+		cout<<"beam_diameter	:"<<beam_rad*.0254*2<<"	m"<<endl;
+	}
+	//in case of user want to generete random distributing voronoi 
+	else 
+	{
+		// random number geration function initiation
+		int initiation=0;
+
+		//Regularity parameter 0 % complete irrigulare structure and 100% is honeycomb structure
+		double  e = 0.9;
+		
+		//these values are evaluated from running the code before. the code converge more quicly when srand() functions initiated with these values
+		int good_initiation[20]={2,28,29,37,45,56,70,95,104,126,162,178,205,229,243,268,309,322,326,357};
+		
+		//create arrays holps seeds coordinates 
+		points_x = new double [num_points];
+		points_y = new double [num_points];
+		points_z = new double [num_points];
+
+		//location where the Dxf file and statistics .txt will be stored
+		string str="D:\\master\\results\\9reg\\00\\00_Drawing.dxf";
+		string str_2="D:\\master\\results\\9reg\\00\\00_beam_radius.txt";
+		string file_num;
+		stringstream convert; // stringstream used for the conversion.
+		
+		// this variable will hold density itration 
+		int e_itra=0;
+		double m_rad_1=0.0,m_rad_3=0.0,m_rad_5=0.0,m_rad_7=0.0,m_rad_9=0.0;
+		
+		//we need to generate 20 model automatically and store them in different distinations 
+		while (initiation<20)
+		{
+			e_itra=0;
+			num_itr=0;
+
+			//use the modified MPS to generate vornoi with regularity 90%
+			mps_3d_for_high_regularity(points_x,points_y,points_z,num_points,e*r_reg,num_itr,good_initiation[initiation]);
+			//mps_3d(points_x,points_y,points_z, num_points, e*r_reg,num_itr,initiation);
+			
+			while(e_itra<5)
+			{
+				if(e_itra==0) denesity=.01;
+				if(e_itra==1) denesity=.03;
+				if(e_itra==2) denesity=.05;
+				if(e_itra==3) denesity=.07;
+				if(e_itra==4) denesity=.09;
+
+				convert << initiation;		     // add   the value of Number to the characters in the stream
+				file_num = convert.str();		//  set   Result to the content of the stream
+				
+				//change location of the output file based on random points function initiation number
+				if (initiation<10)
+				{
+					str.replace(24,1,file_num);
+					str_2.replace(24,1,file_num);
+					str.replace(27,1,file_num);
+					str_2.replace(27,1,file_num);
+				}
+				if (initiation>=10)	
+				{
+					str.replace(23,2,file_num);
+					str_2.replace(23,2,file_num);
+					str.replace(26,2,file_num);
+					str_2.replace(26,2,file_num);
+				}
+				cout<<str<<endl;
+				convert.str("");
+				file_num.clear();
+
+				// Get Vornoi vertices using TetGen library
+				tet_gen(num_points,points_x,points_y,points_z,beam_rad,denesity,r_reg,e,initiation,e*r_reg,num_small_lig, num_draw_points,draw_points);
+
+				// sum the beam diameter for every density and at the end calculate the mean for 20 model
+				if(e_itra==0) m_rad_1=m_rad_1+beam_rad;
+				if(e_itra==1) m_rad_3=m_rad_3+beam_rad;
+				if(e_itra==2) m_rad_5=m_rad_5+beam_rad;
+				if(e_itra==3) m_rad_7=m_rad_7+beam_rad;
+				if(e_itra==4)m_rad_9=m_rad_9+beam_rad;
+
+				// Draw Voronoi Seeds for the first itration of densities as the structure will not change by changing densities 
+				// only Vornoi legaments diameter will change 
+				if(e_itra==0) draw_cells(draw_points,num_draw_points,str);
+
+				if(e_itra==0){ofstream beam_radius (str_2, ios::out);
+				beam_radius<<"denesity	"<<denesity<<endl;
+				beam_radius<<"beam_rad	"<<beam_rad<<"	inch"<<endl;
+				beam_radius<<"num_small_lig	"<<num_small_lig<<endl;
+				beam_radius<<"regularity	"<<e<<endl;
+				beam_radius<<"***********************"<<endl;}
+
+				else	{ofstream beam_radius (str_2, ios::app);
+				beam_radius<<"denesity	"<<denesity<<endl;
+				beam_radius<<"beam_rad	"<<beam_rad<<"	inch"<<endl;
+				beam_radius<<"num_small_lig	"<<num_small_lig<<endl;
+				beam_radius<<"regularity	"<<e<<endl;
+				beam_radius<<"***********************"<<endl;}
+
+				e_itra++;
+			}
+			initiation++;
+		}
+
+	
+		// calculate the mean of Voronoi ligaments for 20 model and store them in dxf file 
+		ofstream beam_radius ("D:\\master\\results\\9reg\\00_beam_radius.txt", ios::out);
+		beam_radius<<"regularity	"<<e<<endl;
+		beam_radius<<"m_rad_1	"<<m_rad_1/initiation<<endl;
+		beam_radius<<"m_rad_3	"<<m_rad_3/initiation<<endl;
+		beam_radius<<"m_rad_5	"<<m_rad_5/initiation<<endl;
+		beam_radius<<"m_rad_7	"<<m_rad_7/initiation<<endl;
+		beam_radius<<"m_rad_9	"<<m_rad_9/initiation<<endl;
+
+	}
+
+	system("pause");
+}
 
 //function checks the number whether it odd or even
 int odd (int a)
@@ -1031,170 +1227,4 @@ void  mps_3d_for_high_regularity(double *points_x,double *points_y,double *point
 	int check_ini;
 	check_ini=seedstracking_lastparent(hit,points_x,points_y, points_z,r);
 
-}
-
-void main()
-{	
-	// creat array that holds voroni vertices 
-	double * draw_points;
-	draw_points= new double [60000000];
-	int num_draw_points=0;
-	
-	//Create a parameter to check whether the user want to generate a honeycomb structure 1 or random voronoi diagram 
-	int reg_type = 1;
-	
-	//the required number of seed inside the unite cube 
-	int num_points=559;
-	int num_itr=1;
-	double* points_x, * points_y, * points_z;
-	
-	// Create variables that holds Vornoi ligaments characteristics
-	double beam_rad=0.0;
-	int num_small_lig=0;
-	double denesity;
-
-	// calculate the required distance to generate complete regulare voronoi structure for "num_points" seeds in unite cube "_x0*_x0*_x0"
-	double r_reg;
-	r_reg=min_dist_req_3d(_x0*_x0*_x0,num_points);
-
-	//in case of user want to generate complete regulare structure "honeycomb"
-	if(reg_type==1)
-	{
-		// set the the required density of model
-		denesity=0.03;
-
-		// Create arrays holds seeds coordinates 
-		points_x = new double [num_points];
-		points_y = new double [num_points];
-		points_z = new double [num_points];
-
-		// generate regulare distribution seeds 
-		regular_points_seeds(points_x,points_y,points_z,r_reg);
-
-		// Creating Vornoi vertices for honeycomb structure using TetGen library 
-		tet_gen_honeycomb(num_points,points_x,points_y,points_z,beam_rad, denesity,num_draw_points,draw_points);
-
-		//set the destination for storing DXF file 
-		string str="D:\\master\\results\\1reg\\00_Drawing.dxf";
-
-		// Draw Honeycomb structure in dxf file
-		draw_cells(draw_points,num_draw_points,str);
-
-		//display voronoi ligaments radiaus in inch and in m
-		cout<<"beam_rad	:"<<beam_rad<<"	inch"<<endl;
-		cout<<"beam_diameter	:"<<beam_rad*.0254*2<<"	m"<<endl;
-	}
-	//in case of user want to generete random distributing voronoi 
-	else 
-	{
-		// random number geration function initiation
-		int initiation=0;
-
-		//Regularity parameter 0 % complete irrigulare structure and 100% is honeycomb structure
-		double  e = 0.9;
-		
-		//these values are evaluated from running the code before. the code converge more quicly when srand() functions initiated with these values
-		int good_initiation[20]={2,28,29,37,45,56,70,95,104,126,162,178,205,229,243,268,309,322,326,357};
-		
-		//create arrays holps seeds coordinates 
-		points_x = new double [num_points];
-		points_y = new double [num_points];
-		points_z = new double [num_points];
-
-		//location where the Dxf file and statistics .txt will be stored
-		string str="D:\\master\\results\\9reg\\00\\00_Drawing.dxf";
-		string str_2="D:\\master\\results\\9reg\\00\\00_beam_radius.txt";
-		string file_num;
-		stringstream convert; // stringstream used for the conversion.
-		
-		// this variable will hold density itration 
-		int e_itra=0;
-		double m_rad_1=0.0,m_rad_3=0.0,m_rad_5=0.0,m_rad_7=0.0,m_rad_9=0.0;
-		
-		//we need to generate 20 model automatically and store them in different distinations 
-		while (initiation<20)
-		{
-			e_itra=0;
-			num_itr=0;
-
-			//use the modified MPS to generate vornoi with regularity 90%
-			mps_3d_for_high_regularity(points_x,points_y,points_z,num_points,e*r_reg,num_itr,good_initiation[initiation]);
-			//mps_3d(points_x,points_y,points_z, num_points, e*r_reg,num_itr,initiation);
-			
-			while(e_itra<5)
-			{
-				if(e_itra==0) denesity=.01;
-				if(e_itra==1) denesity=.03;
-				if(e_itra==2) denesity=.05;
-				if(e_itra==3) denesity=.07;
-				if(e_itra==4) denesity=.09;
-
-				convert << initiation;		     // add   the value of Number to the characters in the stream
-				file_num = convert.str();		//  set   Result to the content of the stream
-				
-				//change location of the output file based on random points function initiation number
-				if (initiation<10)
-				{
-					str.replace(24,1,file_num);
-					str_2.replace(24,1,file_num);
-					str.replace(27,1,file_num);
-					str_2.replace(27,1,file_num);
-				}
-				if (initiation>=10)	
-				{
-					str.replace(23,2,file_num);
-					str_2.replace(23,2,file_num);
-					str.replace(26,2,file_num);
-					str_2.replace(26,2,file_num);
-				}
-				cout<<str<<endl;
-				convert.str("");
-				file_num.clear();
-
-				// Get Vornoi vertices using TetGen library
-				tet_gen(num_points,points_x,points_y,points_z,beam_rad,denesity,r_reg,e,initiation,e*r_reg,num_small_lig, num_draw_points,draw_points);
-
-				// sum the beam diameter for every density and at the end calculate the mean for 20 model
-				if(e_itra==0) m_rad_1=m_rad_1+beam_rad;
-				if(e_itra==1) m_rad_3=m_rad_3+beam_rad;
-				if(e_itra==2) m_rad_5=m_rad_5+beam_rad;
-				if(e_itra==3) m_rad_7=m_rad_7+beam_rad;
-				if(e_itra==4)m_rad_9=m_rad_9+beam_rad;
-
-				// Draw Voronoi Seeds for the first itration of densities as the structure will not change by changing densities 
-				// only Vornoi legaments diameter will change 
-				if(e_itra==0) draw_cells(draw_points,num_draw_points,str);
-
-				if(e_itra==0){ofstream beam_radius (str_2, ios::out);
-				beam_radius<<"denesity	"<<denesity<<endl;
-				beam_radius<<"beam_rad	"<<beam_rad<<"	inch"<<endl;
-				beam_radius<<"num_small_lig	"<<num_small_lig<<endl;
-				beam_radius<<"regularity	"<<e<<endl;
-				beam_radius<<"***********************"<<endl;}
-
-				else	{ofstream beam_radius (str_2, ios::app);
-				beam_radius<<"denesity	"<<denesity<<endl;
-				beam_radius<<"beam_rad	"<<beam_rad<<"	inch"<<endl;
-				beam_radius<<"num_small_lig	"<<num_small_lig<<endl;
-				beam_radius<<"regularity	"<<e<<endl;
-				beam_radius<<"***********************"<<endl;}
-
-				e_itra++;
-			}
-			initiation++;
-		}
-
-	
-		// calculate the mean of Voronoi ligaments for 20 model and store them in dxf file 
-		ofstream beam_radius ("D:\\master\\results\\9reg\\00_beam_radius.txt", ios::out);
-		beam_radius<<"regularity	"<<e<<endl;
-		beam_radius<<"m_rad_1	"<<m_rad_1/initiation<<endl;
-		beam_radius<<"m_rad_3	"<<m_rad_3/initiation<<endl;
-		beam_radius<<"m_rad_5	"<<m_rad_5/initiation<<endl;
-		beam_radius<<"m_rad_7	"<<m_rad_7/initiation<<endl;
-		beam_radius<<"m_rad_9	"<<m_rad_9/initiation<<endl;
-
-	}
-
-	system("pause");
 }
